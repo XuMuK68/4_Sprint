5cc99e2b2fe8869e64f2ad4b2fb1da9e8088f802
Alexander Gavrilov <XuMuK6868@ya.ru>
2026-08-18 14:56:26 +0300

    4 sprint wait rewiew

--- src/main/java/org/example/pageObject/HomePage.java
+++ src/main/java/org/example/pageObject/HomePage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-
 public class HomePage {
 
     private final WebDriver driver;
